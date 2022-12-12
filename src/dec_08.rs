0eec45ad3c1650f3bd9b0563db5a33d9b9b4943e
Josiah Dahl <josiah.dahl@gmail.com>
2022-12-11 23:16:02 -0800

    Dec 08 part two
    Straight up copy and paste then modification. I think it would be
    possible to use the same function to get each direction and then do
    a check outside the function, but w/e.
    
    Ran into some issues with checking the edges (which are always 0). Also
    had a stupid copy and paste error where I was subtracting the x instead
    of the y. Figured it out in the end :)

--- src/dec_08.rs
+++ src/dec_08.rs
@@ -1,4 +1,4 @@
-use std::str::Lines;
+use std::{cmp, str::Lines};
 
 pub fn part_one(mut lines: Lines) {
     let mut forest: Vec<Vec<usize>> = Vec::new();
@@ -23,18 +23,6 @@ pub fn part_one(mut lines: Lines) {
                 visible_count += 1;
                 continue;
             }
-            // Visible from top
-            // visible from right
-            // visible from bottom
-            // Visible from left
-            // x T x
-            // L t R
-            // x B x
-            // let top = &forest[y - 1][x];
-            // let right = &forest[y][x + 1];
-            // let bottom = &forest[y + 1][x];
-            // let left = &forest[y][x - 1];
-
             if visible_up(&forest, tree, x, y) {
                 // println!("{tree} at ({x}, {y}) is visible from up");
                 visible_count += 1;
@@ -111,4 +99,86 @@ fn visible_left(forest: &Vec<Vec<usize>>, tree_val: &usize, tree_x: usize, tree_
     // println!("visible_left: {tree_val} is visible on row {tree_y} at col {tree_x}");
     return true;
 }
-pub fn part_two(_lines: Lines) {}
+pub fn part_two(mut lines: Lines) {
+    let mut forest: Vec<Vec<usize>> = Vec::new();
+
+    while let Some(line) = lines.next() {
+        let mut row: Vec<usize> = Vec::new();
+        for c in line.chars() {
+            row.push(c.to_digit(10).unwrap() as usize);
+        }
+        forest.push(row);
+    }
+    let mut max_scenic_score = std::usize::MIN;
+    for (y, row) in forest.iter().enumerate() {
+        for (x, tree) in row.iter().enumerate() {
+            let up = scenic_up(&forest, tree, x, y);
+            let right = scenic_right(&forest, tree, x, y);
+            let down = scenic_down(&forest, tree, x, y);
+            let left = scenic_left(&forest, tree, x, y);
+            let scenic_score = up * right * down * left;
+            // println!("({x}, {y}) score: {up} {right} {down} {left} = {scenic_score}");
+            if scenic_score > max_scenic_score {
+                max_scenic_score = scenic_score;
+                println!("Found {scenic_score} at ({x}, {y}) with {up} {right} {down} {left}");
+            }
+            // max_scenic_score = cmp::max(max_scenic_score, scenic_score);
+        }
+    }
+    println!("Part Two: Max Scenic Score {max_scenic_score}");
+}
+// Scenic algorithm
+// 0. If you are on an edge and going in that direction, scenic is 0
+// 1. Look in the direction until you find a tree that is same height or taller.
+// 2. Scenic is tree_coord - bigger_coord
+fn scenic_up(forest: &Vec<Vec<usize>>, tree_val: &usize, tree_x: usize, tree_y: usize) -> usize {
+    if tree_y == std::usize::MIN {
+        return 0;
+    }
+    // The .rev() thing is weird, this goes from tree_y - 1 down to 0
+    for cur_row in (std::usize::MIN..=(tree_y - 1)).rev() {
+        let up_val = &forest[cur_row][tree_x];
+        if up_val >= &tree_val {
+            return tree_y - cur_row;
+        }
+    }
+
+    return tree_y;
+}
+
+fn scenic_right(forest: &Vec<Vec<usize>>, tree_val: &usize, tree_x: usize, tree_y: usize) -> usize {
+    if tree_x == forest[0].len() - 1 {
+        return 0;
+    }
+    for cur_col in (tree_x + 1)..forest[0].len() {
+        let right_val = &forest[tree_y][cur_col];
+        if right_val >= &tree_val {
+            return cur_col - tree_x;
+        }
+    }
+    return forest[0].len() - 1 - tree_x;
+}
+fn scenic_down(forest: &Vec<Vec<usize>>, tree_val: &usize, tree_x: usize, tree_y: usize) -> usize {
+    if tree_y == forest.len() - 1 {
+        return 0;
+    }
+    for cur_row in (tree_y + 1)..forest.len() {
+        let down_val = &forest[cur_row][tree_x];
+        if down_val >= &tree_val {
+            return cur_row - tree_y;
+        }
+    }
+    return forest.len() - 1 - tree_y;
+}
+fn scenic_left(forest: &Vec<Vec<usize>>, tree_val: &usize, tree_x: usize, tree_y: usize) -> usize {
+    if tree_x == 0 {
+        return 0;
+    }
+    for cur_col in (std::usize::MIN..=(tree_x - 1)).rev() {
+        let left_val = &forest[tree_y][cur_col];
+        if left_val >= &tree_val {
+            return tree_x - cur_col;
+        }
+    }
+    return tree_x;
+}
